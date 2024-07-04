cad5a53dc1f5c1530ff543c44e8de26aa7e40b78
Omer Yesil <oyesil@gmail.com>
2024-07-04 00:56:17 -0400

    Updated sample python script

--- nifi.2.0.0.m3/sample-scripts/company_a/findMontreal.py
+++ nifi.2.0.0.m3/sample-scripts/company_a/findMontreal.py
@@ -14,23 +14,33 @@ class PyStreamCallback(StreamCallback):
         # Convert the content to JSON
         json_content = json.loads(input_text)
 
-        # Get the length of the JSON array
-        json_length = len(json_content)
-
-        # Log the length of the JSON array
-        print("Length of JSON array:", json_length)
-
         # Find the record with city "Montreal"
         record_with_montreal = next((record for record in json_content if record.get("city") == "Montreal"), None)
-          
+
         if record_with_montreal:
             output_text = json.dumps(record_with_montreal)
             outputStream.write(output_text.encode('utf-8'))
-
         else:
             raise ValueError('No record found with city "Montreal"')
 
+class ErrorStreamCallback(StreamCallback):
+    def __init__(self):
+        pass
+
+    def process(self, inputStream, outputStream):
+        error_message = {"error": "Montreal is not found"}
+        outputStream.write(json.dumps(error_message).encode('utf-8'))
+
 flowFile = session.get()
 if flowFile is not None:
-    flowFile = session.write(flowFile, PyStreamCallback())
-    session.transfer(flowFile, REL_SUCCESS)
+    try:
+        flowFile = session.write(flowFile, PyStreamCallback())
+        session.transfer(flowFile, REL_SUCCESS)
+    except Exception as e:
+        # Create a new flowfile for the error message
+        errorFlowFile = session.create()
+        errorFlowFile = session.write(errorFlowFile, ErrorStreamCallback())
+        # Set the content type to application/json
+        errorFlowFile = session.putAttribute(errorFlowFile, "mime.type", "application/json")
+        session.transfer(errorFlowFile, REL_FAILURE)
+        session.remove(flowFile)
